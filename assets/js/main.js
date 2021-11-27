12d320095ca665c2cb038c5e89f74e8084d7736a
Grit <perepadin00@gmail.com>
2021-11-27 18:26:38 +0300

    create products section

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,4 +35,3 @@ function scrollHeader(){
     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
-
